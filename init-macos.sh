52fb999e545d6aac35604ebc14dc79a2a44176e8
Stephan Lagerwaard <stephanlagerwaard@gmail.com>
2025-10-21 19:58:04 +0200

    support arch

---
+++ init-macos.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "initializing dotfiles for macos"
+
+echo "installing neovim, pnpm, and rust"
+brew install neovim pnpm rustup
+
+echo "adding rust components"
+rustup component add rustfmt clippy
+
+echo "setting up astronvim config"
+# cp -r $(pwd)/.config/ ~/
+# ln -sfn $(pwd)/.config ~/.config
+
+echo "setting up zsh themes"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+curl https://raw.githubusercontent.com/sainnhe/dotfiles/master/.zsh-theme-everforest-dark --output .zsh-theme-everforest-dark
+
+echo "symlinking dotfiles"
+# ln -sfn $(pwd)/.gitconfig $HOME/.gitconfig
+# ln -sfn $(pwd)/.gitexcludes $HOME/.gitexcludes
+# ln -sfn $(pwd)/.zshrc $HOME/.zshrc
+# ln -sfn $(pwd)/.zsh-theme-everforest-dark $HOME/.zsh-theme-everforest-dark
+# ln -sfn $(pwd)/.npmrc $HOME/.npmrc
+
+echo "macos initialization complete"
