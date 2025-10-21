835992ddd17f15d4978c571deac29d92a2fd8b6d
Stephan Lagerwaard <stephanlagerwaard@gmail.com>
2025-10-21 21:24:27 +0200

    move nvim deps to nvim init

--- nvim/init.sh
+++ nvim/init.sh
@@ -3,11 +3,11 @@
 echo "initializing nvim"
 
 if command -v pacman &> /dev/null; then
-    sudo pacman -S --noconfirm neovim
+    sudo pacman -S --noconfirm neovim python ripgrep tree-sitter xclip bottom gdu
 elif command -v brew &> /dev/null; then
-    brew install --yes neovim
+    brew install --yes neovim python ripgrep tree-sitter xclip bottom gdu
 else
-    echo "unsupported package manager. please install neovim manually."
+    echo "unsupported package manager. please install neovim, python, ripgrep, tree-sitter, xclip, bottom, gdu manually."
     exit 1
 fi
 
--- system/init.sh
+++ system/init.sh
@@ -4,13 +4,11 @@ echo "updating system packages"
 
 if command -v pacman &> /dev/null; then
     sudo pacman -Syu --noconfirm
-    sudo pacman -S --noconfirm python ripgrep tree-sitter xclip bottom gdu
 elif command -v brew &> /dev/null; then
     brew update
     brew upgrade
-    brew install python ripgrep tree-sitter xclip bottom gdu
 else
-    echo "unsupported package manager. please update and install python, ripgrep, tree-sitter, xclip, lazygit, bottom, gdu manually."
+    echo "unsupported package manager. please update manually."
     exit 1
 fi
 
