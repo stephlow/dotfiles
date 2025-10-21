b477a7fdbb03750e1c705b3a37eaaeb0f328e330
Stephan Lagerwaard <stephanlagerwaard@gmail.com>
2025-10-21 21:28:26 +0200

    oh-my-zsh installer improvements

--- zsh/init.sh
+++ zsh/init.sh
@@ -2,6 +2,15 @@
 
 echo "setting up zsh"
 
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    echo "installing oh-my-zsh"
+    if command -v yay &> /dev/null; then
+        yay -S oh-my-zsh-git --noconfirm
+    else
+        RUNZSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    fi
+fi
+
 echo "symlinking zsh files"
 ln -sfn $(pwd)/zsh/config/.zshrc ~/.zshrc
 ln -sfn $(pwd)/zsh/config/.zsh-theme-everforest-dark ~/.zsh-theme-everforest-dark
