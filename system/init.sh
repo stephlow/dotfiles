a03998cf4282ded56980feb3966028b82fdbdbc7
Stephan Lagerwaard <stephanlagerwaard@gmail.com>
2025-10-21 21:08:39 +0200

    split system init with pacman

--- system/init.sh
+++ system/init.sh
@@ -3,7 +3,8 @@
 echo "updating system packages"
 
 if command -v pacman &> /dev/null; then
-    sudo pacman -Syu --noconfirm python ripgrep tree-sitter xclip bottom gdu
+    sudo pacman -Syu --noconfirm
+    sudo pacman -S --noconfirm python ripgrep tree-sitter xclip bottom gdu
 elif command -v brew &> /dev/null; then
     brew update
     brew upgrade
